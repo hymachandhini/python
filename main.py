7fe04eb8955a92d2f29ef69317ffab802ff9ac45
Namana <hymachandhininamana@gmail.com>
2024-08-30 14:10:56 +0530

    First day python project

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-print("Welcome to Jenkins project"[D[D[D[D[D[D[D[D[D[C[P[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[P[C[C[C[C[C[C[C[C[C[D[D)
+print("Welcome to jenkin project")
